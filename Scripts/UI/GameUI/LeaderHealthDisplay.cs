b1edd19be5f38efc043d2e1f506df9ef2b9922a2
Sparkfire <48664233+Sparkfir3@users.noreply.github.com>
2026-02-18 23:45:31 -0600

    Stat change animation & color for leader health

--- Scripts/UI/GameUI/LeaderHealthDisplay.cs
+++ Scripts/UI/GameUI/LeaderHealthDisplay.cs
@@ -9,21 +9,38 @@ namespace SVESimulator
     {
         [SerializeField]
         private TextMeshProUGUI defenseText;
+        [SerializeField, InlineEditor]
+        private CardStatColorSettings statColorSettings;
+
+        private int baseDefense = 20;
 
         // ------------------------------
 
         public void Initialize(Stat playerDefenseStat)
         {
-            SetDefense(playerDefenseStat.effectiveValue);
+            baseDefense = playerDefenseStat.effectiveValue;
+            SetDefense(baseDefense);
             playerDefenseStat.onValueChanged += (oldAmount, newAmount) =>
             {
-                SetDefense(newAmount);
+                SetDefense(newAmount, true, newAmount - oldAmount);
             };
         }
 
-        public void SetDefense(int def)
+        public void SetDefense(int def, bool playAnimation = false, int difference = 0)
         {
             defenseText.text = def.ToString();
+            if(def < baseDefense)
+            {
+                defenseText.color = statColorSettings.StatDownColor;
+                defenseText.fontSharedMaterial = statColorSettings.WhiteOutlineMaterial;
+            }
+            else
+            {
+                defenseText.color = def == baseDefense ? statColorSettings.StatBaseColor : statColorSettings.StatBuffedColor;
+                defenseText.fontSharedMaterial = statColorSettings.BlackOutlineMaterial;
+            }
+            if(playAnimation)
+                CardManager.Animator.PlayStatChangeAnimation(defenseText.transform.position, difference);
         }
     }
 }
