6a63c1a5ef076a8e80e27b3107510973ce3ac737
Sparkfire <48664233+Sparkfir3@users.noreply.github.com>
2026-08-08 20:18:51 -0500

    Add startup delay to sequence (fix optional prompt in XAndTarget-type effects)
    Fix race condition between close targeting menu & open pay cost/yes or no menu
    + Update card text

--- Scripts/Gameplay/CardAbilities/Effects/SequencingEffects/OrderedSequencing/EffectSequence.cs
+++ Scripts/Gameplay/CardAbilities/Effects/SequencingEffects/OrderedSequencing/EffectSequence.cs
@@ -36,6 +36,7 @@ namespace SVESimulator
         public static IEnumerator ResolveEffectsAsSequence(List<string> effectList, PlayerController player, int triggeringCardInstanceId, string triggeringCardZone, int sourceCardInstanceId, string sourceCardZone,
             Action onComplete, string additionalFilters = null, string overrideAmount = null, bool ignoreCosts = false)
         {
+            yield return new WaitForEndOfFrame();
             CardObject cardObject = CardManager.Instance.GetCardByInstanceId(sourceCardInstanceId);
             Card libraryCard = LibraryCardCache.GetCard(cardObject.RuntimeCard.cardId, GameManager.Instance.config);
             List<Ability> sveAbilities = libraryCard.abilities.FindAll(x => x is TriggeredAbility { effect: SveEffect });
